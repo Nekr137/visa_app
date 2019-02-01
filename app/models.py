dc1f6876fdf0b3295dfd35b8613163e226e56ae3
Denis <denekra@yandex.ru>
2019-02-01 14:07:57 +0700

    немного подчистил class Data

--- app/models.py
+++ app/models.py
@@ -3,7 +3,5 @@ from django.db import models
 
 # таблица app_data
 class Data(models.Model):
-    # name = models.CharField(max_length=21)
-    # age = models.IntegerField()
     grazhdanstvo = models.CharField(max_length=30)
     kratnost = models.CharField(max_length=30)
