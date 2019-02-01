bfad9303388b3dda6f55776d9270601d512cae79
Denis <denekra@yandex.ru>
2019-02-01 14:44:37 +0700

    add fields

--- app/models.py
+++ app/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Data(models.Model):
     grazhdanstvo = models.CharField(max_length=30)
     kratnost = models.CharField(max_length=30)
+
+    familiya = models.CharField(max_length=30)
+    imya = models.CharField(max_length=30)
+    data_rozh = models.CharField(max_length=30)
+    nomer_pass = models.IntegerField()
+    grazhdanstvo2 = models.CharField(max_length=30)
