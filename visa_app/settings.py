15214eb18cadd8a169483d4bbc95643da9e69dd1
Denis <denekra@yandex.ru>
2019-02-19 07:43:52 +0700

    Убрал поля в документе xlsx

--- visa_app/settings.py
+++ visa_app/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'visa_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'testdb',
-        #'USER':'postgres',
-        #'PASSWORD':'denek',
-        'NAME' : 'django_db',
-        'USER' : 'postgres',
-        'PASSWORD' : 'postgres',
+        'NAME': 'testdb',
+        'USER':'postgres',
+        'PASSWORD':'denekra',
+        #'NAME' : 'django_db',
+        #'USER' : 'postgres',
+        #'PASSWORD' : 'postgres',
         'HOST':'127.0.0.1',
         'PORT':'5432',
     }
